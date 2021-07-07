b58a8292ed1f62c207cf66ce973ad94bca89260e
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-07 22:15:26 +0530

    Update linters and dependencies

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is the default React page
+          This is the default React page
         </p>
         <a
           className="App-link"
