b33bf475d75b48cef5acc7dfab6fb2dc6699db42
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-14 01:52:54 +0530

    Add calculate.js to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import calculate from '../logic/calculate';
 
 function App() {
+  const data = { total: '20', next: '5', operation: '+' };
+
+  const buttonname = '+/-';
+
+  calculate(data, buttonname);
+
   return (
-    <div>
+    <>
       <ButtonPanel />
       <br />
       <Display result="25" />
-    </div>
+
+    </>
   );
 }
 
