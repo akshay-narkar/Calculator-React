f5e527e5cefa8b861dee5d51cf3da3ff61f4cefc
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-20 21:25:54 +0530

    Convert app dot js to functional component with state using usestate hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+function App() {
+  const [state, setState1] = useState({
+    total: null,
+    next: null,
+    operaion: null,
+  });
 
-  handleClick = (buttonName) => {
-    const dataObject = { ...this.state };
+  const handleClick = (buttonName) => {
+    const dataObject = { ...state };
+
     if (dataObject.total === 'error') {
-      this.setState(() => ({
+      setState1({
         total: null,
         next: null,
         operation: null,
-      }));
+      });
       return;
     }
-    const { next, operation, total } = calculate(dataObject, buttonName);
-    this.setState(() => (
-      {
-        next,
-        total,
-        operation,
-      }
-    ));
-  }
 
-  render() {
-    const { total } = this.state;
-    return (
-      <div className="container">
-        <Display result={total} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
+    const { next, operation, total } = calculate(dataObject, buttonName);
+    setState1({
+      next,
+      operation,
+      total,
+    });
+  };
+  const { total } = state;
+  return (
+    <div className="container">
+      <Display result={total} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
 }
+
+export default App;
+
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       total: null,
+//       next: null,
+//       operation: null,
+//     };
+//   }
+
+// handleClick = (buttonName) => {
+//   const dataObject = { ...this.state };
+//   if (dataObject.total === 'error') {
+//     this.setState(() => ({
+//       total: null,
+//       next: null,
+//       operation: null,
+//     }));
+//     return;
+//   }
+//   const { next, operation, total } = calculate(dataObject, buttonName);
+//   this.setState(() => (
+//     {
+//       next,
+//       total,
+//       operation,
+//     }
+//   ));
+// }
+
+//   render() {
+//     const { total } = this.state;
+//     return (
+//       <div className="container">
+//         <Display result={total} />
+//         <ButtonPanel clickHandler={this.handleClick} />
+//       </div>
+//     );
+//   }
+// }
