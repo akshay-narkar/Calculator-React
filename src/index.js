dd52dff14bf7e52cfb8c3f56212eaf184c91d04a
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-22 00:14:51 +0530

    Update index using Browser route and link to Homeroutes component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
-import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/Homeroutes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <Home />
+
+  </BrowserRouter>,
   document.getElementById('root'),
 );
