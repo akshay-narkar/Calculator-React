cfd331c5062c4681ca54e08241983c79ccdc8334
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-22 00:12:40 +0530

    Remove commented code

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,12 +2,11 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 function Display(props) {
-  // const [result] = props;
+  const { result } = props; // destructoring
   const text = useRef(null);
   useEffect(() => {
-    const { result } = props;
+    const { result } = props; // destructoring
     const node = text.current;
-    // const node = text.current;
     node.style.fontSize = '40px';
     const width = node.scrollWidth;
     const overflowWidth = width - 320;
@@ -19,7 +18,6 @@ function Display(props) {
     }
   });
 
-  const { result } = props;
   return (
     <div className="display">
       <span ref={text} id="text">{result}</span>
@@ -28,36 +26,6 @@ function Display(props) {
 }
 
 export default Display;
-// export default class Display extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.text = React.createRef();
-//   }
-
-//   componentDidUpdate() {
-//     const { result } = this.props;
-//     const node = this.text.current;
-//     node.style.fontSize = '40px';
-//     const width = node.scrollWidth;
-//     const overflowWidth = width - 320;
-//     if (result) {
-//       const resultLength = result.length;
-//       if (overflowWidth > 0 && resultLength > 16) {
-//         node.style.fontSize = '30px';
-//       }
-//     }
-//   }
-
-//   render() {
-//     const { result } = this.props;
-//     return (
-//       <div className="display">
-//         <span ref={this.text} id="text">{result}</span>
-//       </div>
-//     );
-//   }
-// }
-
 Display.propTypes = {
   result: PropTypes.string,
 };
