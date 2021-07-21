effc412fd9d36a5afbd5a32d5aeae462eeaa4059
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-22 00:11:51 +0530

    Create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  const styling = {
+    textDecoration: 'none',
+    color: 'black',
+    display: 'inline',
+  };
+
+  const spancolor = {
+    color: 'white',
+    fontSize: 25,
+
+  };
+
+  return (
+    <header>
+      <nav>
+        <div>
+          <Link style={styling} to="/">
+            <h4 className="headername">
+              Math Magicians
+            </h4>
+          </Link>
+        </div>
+        <ul className="ulnav">
+          <li style={styling}>
+            <Link className="textinlink" to="/">
+              Home
+            </Link>
+            <span style={spancolor}> | </span>
+          </li>
+          <li style={styling}>
+            <Link className="textinlink" to="/app">
+              Calculator
+            </Link>
+            <span style={spancolor}> | </span>
+          </li>
+          <li style={styling}>
+            <Link className="textinlink" to="/quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+  );
+}
