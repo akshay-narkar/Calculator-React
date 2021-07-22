23702dd09615edf1ef1a481566b9d3ab5b70e7e4
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-22 18:26:26 +0530

    Remove linter errors

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,10 +1,8 @@
 import renderer from 'react-test-renderer';
-import App from '../components/App'
+import App from '../components/App';
 
 it('UI is unaltered wrt first snapshot', () => {
-
   const elem = renderer.create(<App />).toJSON();
 
   expect(elem).toMatchSnapshot();
-        
 });
